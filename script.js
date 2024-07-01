87387ff4d762fd3567aaad4e7c2038b80c105a98
vksaless <vksales76@gmail.com>
2024-07-01 03:44:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     event.preventDefault();
     alert('Mensagem enviada com sucesso!');
 });
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('http://localhost:3000/api/places')
+        .then(response => response.json())
+        .then(data => {
+            const placeList = document.getElementById('place-list');
+            data.forEach(place => {
+                const placeItem = document.createElement('div');
+                placeItem.innerHTML = `<h3>${place.name}</h3><p>${place.address}</p><p>${place.contact}</p><p>${place.hours}</p>`;
+                placeList.appendChild(placeItem);
+            });
+        });
+});
